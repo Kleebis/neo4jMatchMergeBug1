ceed41c2780804542cdbced1cb2813bae69c4a42
Jan Brogger <5682500+janbrogger@users.noreply.github.com>
2020-11-22 09:26:49 +0100

    Example query runs. No bug!

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const neo4j = require('neo4j-driver');
 const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'pwd'));
 const session = driver.session();
-const queryString = `MATCH (User:User) MATCH (Session:sessions) `
+const queryString1 = `MATCH (n) DELETE n;`;
+const queryString2 = `CREATE ( User: Users { UserName: "admin" });`;
+const queryString3 = `CREATE ( Session: Sessions { sessionId: "sessionId123" });`;
+const queryString4 = `MATCH (User:Users) MATCH (Session:Sessions) `
   + `WHERE User.UserName = "admin" AND Session.sessionId = "sessionId123" `
   + 'CREATE (User)-[r:SESSION_USER]->(Session); ';
 
 session.writeTransaction((tx) => {
-  tx.run(queryString)
-    .then((neo4jresult) => {
-      console.log(`relationships created:  ${neo4jresult.summary.counters._stats.relationshipsCreated}`);              
-    })
-    .catch((error) => {
-      console.log(`Neo4j error: ${JSON.stringify(error.message)}`);
-    })
-    .finally(() => {
-      session.close();
-      driver.close();
-    });
+  tx.run(queryString1)
+    .then(tx.run(queryString2)
+      .then(tx.run(queryString3)
+        .then(tx.run(queryString4)
+      )
+    )
+  );
+})
+.finally(() => {
+  session.close();
+  driver.close();
 });
 
 
